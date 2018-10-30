ac261c8113432c5a7cab1656c492cfd8dd8d7141
jaebooker <jaesonbooker@gmail.com>
2018-10-29 23:44:29 -0700

    added stuff and things

--- models/posts.js
+++ models/posts.js
@@ -2,9 +2,22 @@ const mongoose = requore("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
     title: { type: String, required: true},
     uri: { type: String, required: true },
     summary: { type: String, required: true }
+});
+
+PostSchema.pre("save", function(next) {
+    //createdAt and updatedAt
+    const now = new Date();
+    this.updatedAt = now;
+
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
 });
 
 module.exports = mongoose.model("Post", PostSchema);
--- server.js
+++ server.js
@@ -5,10 +5,25 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const posts = require('./controllers/posts.js')(app);
 const db = require('./data/reddit-clone-db');
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    "mongodb://localhost/redditclone",
+    { useMongoClient: true }
+);
+mongoose.connection.on("error", console.error.bind(console, "failed to connect to the data: "));
+mongoose.set('debug', true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlended({ extended: false }));
 app.use(expressValidator());
-app.get('/', (req, res) => res.send('Good Morning, Starshine, the Earth says Hello!'))
+app.get('/', (req, res) => {
+    Post.find({})
+    .then(posts => {
+        res.render("posts-index", { posts });
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+})
 app.get('/posts/new', (req, res) => {
     res.render('posts-new', {});
 })
